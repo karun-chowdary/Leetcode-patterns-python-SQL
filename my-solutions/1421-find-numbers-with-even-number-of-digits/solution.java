df8a00cc4aad1bc927a47e82eb666d0b31e16889
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2022-09-02 10:40:03 +0000

    Sync LeetCode submission Runtime - 2 ms (33.04%), Memory - 42.9 MB (99.98%)

--- my-solutions/1421-find-numbers-with-even-number-of-digits/solution.java
+++ my-solutions/1421-find-numbers-with-even-number-of-digits/solution.java
@@ -1,15 +1,44 @@
-import java.util.*;
 class Solution {
     public int findNumbers(int[] nums) {
-        int c =0;
-       String[] a = new String[nums.length];
-        for(int i=0;i<nums.length;i++){
-            a[i]=String.valueOf(nums[i]);
-        }
-        for(int i=0;i<a.length;i++){
-            if(a[i].length()%2==0) c+=1;
-        }
-       return c;
+        return find(nums);
+       // return findCount(nums);
         
     }
+    static int findCount(int[] nums) {
+        int count = 0;
+        for (int i :nums) {
+            if (even(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    static boolean even(int n){
+        int noOfDigits = digits(n);
+        return noOfDigits%2==0;
+    }
+    static int digits(int n){
+        if(n<0) n=n*-1;
+        if(n==0) return 1;
+        int count =0;
+        while(n>0){
+            count++;
+            n/=10;
+        }
+        return count;
+    }
+    static int find(int[] nums) {
+        int count = 0;
+        for (int i :nums) {
+            if (digits2(i)%2==0) {
+                count++;
+            }
+        }
+        return count;
+    }
+    static int digits2(int n){
+        if(n<0) n*=-1;
+        return (int)(Math.log10(n)+1);
+    }
+
 }
