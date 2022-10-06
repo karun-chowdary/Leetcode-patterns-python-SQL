03b55ac4d3e2eb2a3ccba02ba79743adf03da341
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2022-10-06 14:17:31 +0000

    Sync LeetCode submission Runtime - 7 ms (70.19%), Memory - 42.9 MB (100.00%)

--- my-solutions/2042-maximum-product-difference-between-two-pairs/solution.java
+++ my-solutions/2042-maximum-product-difference-between-two-pairs/solution.java
@@ -1,10 +1,8 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public int maxProductDifference(int[] nums) {
         Arrays.sort(nums);
-        int i = nums.length-1;
-        return (nums[i]*nums[i-1])-(nums[0]*nums[1]);
+        return (nums[nums.length-1]*nums[nums.length-2]) - (nums[1]*nums[0]);
         
     }
 }
