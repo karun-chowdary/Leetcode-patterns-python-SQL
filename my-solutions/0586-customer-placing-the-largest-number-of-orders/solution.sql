2da7056333fb55a45ecc774fdd91fcc95a6abd8c
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2026-01-29 13:40:59 +0000

    Sync LeetCode submission Runtime - 465 ms (37.32%), Memory - 0.0B (100.00%)

--- my-solutions/0586-customer-placing-the-largest-number-of-orders/solution.sql
+++ my-solutions/0586-customer-placing-the-largest-number-of-orders/solution.sql
@@ -1,6 +1,17 @@
 # Write your MySQL query statement below
-SELECT customer_number
+/*SELECT customer_number
 FROM orders
 GROUP BY customer_number
 ORDER BY count(order_number) DESC
-LIMIT 1
+LIMIT 1*/
+
+SELECT customer_number
+FROM (
+    SELECT customer_number,
+           COUNT(order_number) AS cnt,
+           DENSE_RANK() OVER (ORDER BY COUNT(order_number) DESC) AS rnk
+    FROM orders
+    GROUP BY customer_number
+) t
+WHERE rnk = 1;
+
