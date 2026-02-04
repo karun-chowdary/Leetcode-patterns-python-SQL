44b7db318044cb74ab5f027f61ffd0de1b441fd6
Karun Sai chowdary <109739413+karun-chowdary@users.noreply.github.com>
2026-02-04 19:20:17 +0530

    Day 11 SQL Practice

--- sql-learning/day11.sql
+++ sql-learning/day11.sql
@@ -1,11 +1,4 @@
-Select 
-    round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
-from Delivery
-where (customer_id, order_date) in (
-  Select customer_id, min(order_date) 
-  from Delivery
-  group by customer_id
-);-- Day 11: SQL Practice
+-- Day 11: SQL Practice
 -- Source: LeetCode
 -- Problem: Immediate Food Delivery II
 
