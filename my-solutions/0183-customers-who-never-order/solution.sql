495b040ac1a81173eeb087778ec9fe83e6cbf949
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2026-02-12 04:07:45 +0000

    Sync LeetCode submission Runtime - 690 ms (11.89%), Memory - 0.0B (100.00%)

--- my-solutions/0183-customers-who-never-order/solution.sql
+++ my-solutions/0183-customers-who-never-order/solution.sql
@@ -4,3 +4,18 @@ FROM customers c
 LEFT JOIN orders o
 ON c.id = o.customerid
 WHERE o.id is NULL
+
+#pyspark code
+/* customers_with_no_order = customers.join(
+    orders, 
+    customers.id == orders.id,
+    "left anti").select(customers.name.alias("Customers"))
+    customer_with_no_order.show()*/
+
+#SQL-style left join
+/* customers_with_no_order = customers.alias('c').join(
+    orders.alias('o'),
+    col("c.id") == col("o.id"),
+    "left"
+)..filter(col('o.id').isNULL())
+.select(col('c.name').alias("Customers"))*/
