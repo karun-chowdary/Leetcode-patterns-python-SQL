9cf3eec63bb1b1797098d936962719b14ce9f81c
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2026-02-23 06:46:51 +0530

    Time: 494 ms (95.97%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -3,3 +3,17 @@ SELECT customer_id
 FROM customer
 GROUP BY customer_id
 HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(product_key) FROM product)
+
+#PySpark Query
+/*from pyspark.sql import functions as F
+
+# 1. Get the total number of products as a scalar value
+total_products_count = product.count()
+
+# 2. Group by customer, count unique products, and filter
+result = customer.groupBy("customer_id") \
+    .agg(F.countDistinct("product_key").alias("distinct_products")) \
+    .filter(F.col("distinct_products") == total_products_count) \
+    .select("customer_id")
+
+result.show()*/
