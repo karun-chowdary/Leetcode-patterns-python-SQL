38272f84cc6e4728cf494654447ba2c42544ad9c
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2026-02-28 23:15:11 +0530

    Time: 318 ms (84.62%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,24 @@
+# Write your MySQL query statement below
+/*SELECT
+CASE
+WHEN id%2 = 1 AND id<(SELECT MAX(id) FROM seat) THEN id+1
+WHEN id%2 = 0 THEN id - 1
+ELSE id
+END AS id, student
+FROM seat
+ORDER BY id
+
+SELECT id ,
+COALESCE (CASE 
+WHEN id%2=1 THEN LEAD(student) OVER (ORDER BY id)
+ELSE LAG(student) OVER (ORDER BY id)
+END,
+ student) AS student
+FROM seat*/
+SELECT 
+CASE WHEN id%2=0  THEN id-1
+WHEN (id%2=1 AND id!=(SELECT COUNT(*) FROM seat)) THEN id+1
+ELSE id
+END as id, student
+FROM seat
+ORDER BY id
