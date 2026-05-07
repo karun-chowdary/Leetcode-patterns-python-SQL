587f685b732a3e9002f28a6cb2bec3f2ca9909aa
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2026-05-07 16:13:31 +0530

    Time: 791 ms (33.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -0,0 +1,31 @@
+# Write your MySQL query statement below
+select * from Users
+where regexp_like(mail, '^[A-Za-z]+[A-Za-z0-9_.-]*@leetcode[.]com$','c')
+/*SELECT *
+FROM Users
+WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\\.com$';*/
+
+#pyspark query
+/*from pyspark.sql import functions as F
+result = users_df.filter(F.col("mail").rlike("^[A-Za-z]+[A-Za-z0-9_.-]*@leetcode[.]com$"))
+result_alt = users_df.filter(F.regexp_like(F.col("mail"), "^[A-Za-z]+[A-Za-z0-9_.-]*@leetcode[.]com$"))
+
+result.show()*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
