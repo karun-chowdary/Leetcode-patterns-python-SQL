80a58fcf5791bcb8e0118ee54786c52df1022ecf
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2026-05-13 15:48:47 +0530

    Time: 380 ms (21.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,45 @@
+# Write your MySQL query statement below
+/*SELECT
+CASE
+WHEN id%2 = 1 AND id<(SELECT MAX(id) FROM seat) THEN id+1
+WHEN id%2 = 0 THEN id - 1
+ELSE id
+END AS id, student
+FROM seat
+ORDER BY id*/
+
+SELECT id ,
+COALESCE (CASE 
+WHEN id%2=1 THEN LEAD(student) OVER (ORDER BY id)
+ELSE LAG(student) OVER (ORDER BY id)
+END,
+ student) AS student
+FROM seat
+
+#pysaprk query
+/*from pyspark.sql.window import Window
+
+window_spec = Window.orderBy() # Empty partition = whole table
+result = seat.withColumn("max_id", F.max("id").over(window_spec)) \
+             .withColumn("id", 
+                 F.when((F.col("id") % 2 == 1) & (F.col("id") < F.col("max_id")), F.col("id") + 1)
+                  .when(F.col("id") % 2 == 0, F.col("id") - 1)
+                  .otherwise(F.col("id"))
+             ).drop("max_id").orderBy("id")*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             
